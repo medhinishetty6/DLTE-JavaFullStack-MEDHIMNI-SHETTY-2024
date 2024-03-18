9259af689658075d3c636f08fe72c43c304b966f
medhinishetty6 <shetty.md6@gmail.com>
2024-03-18 17:45:02 +0530

    task uploaded

---
+++ DLTE-JavaEE/Java-Task/861-Rest Get mapping of Transactions/src/main/java/restGetmapping/findAllByUsername.java
@@ -0,0 +1,4 @@
+package restGetmapping;
+
+public class findAllByUsername {
+}
---
+++ DLTE-Spring/DLTE-Spring-001/src/main/java/org/example/Branch.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Branch {
+    private String branchname;
+    private String ifscode;
+    private String branchID;
+    private String bankName;
+    private String bankContact;
+}
