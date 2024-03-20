9e8f331f85d1f140c27d2b508b8d00f23d0957dd
medhinishetty6 <shetty.md6@gmail.com>
2024-03-20 09:14:14 +0530

    task uploaded

---
+++ DLTE-JavaEE/Java-Task/862-RestfulWebservice/src/main/java/restfulweb/findByUserNameDate.java
@@ -0,0 +1,13 @@
+package restfulweb;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+    public class findByUserNameDate {
+        public void doGet(HttpServletRequest req, HttpServletResponse res){
+
+
+
+        }
+    }
+
