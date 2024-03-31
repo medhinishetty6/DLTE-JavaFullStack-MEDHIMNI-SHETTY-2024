67fcb137a3bad9dbad8a304068e772ac46d043d2
medhinishetty6 <shetty.md6@gmail.com>
2024-03-31 19:05:14 +0530

    task uploaded

--- DLTE-Spring/Task/1098-SpringBootJDBC/src/main/java/com/example/demo/TransactionServices.java
+++ DLTE-Spring/Task/1098-SpringBootJDBC/src/main/java/com/example/demo/TransactionServices.java
@@ -14,7 +14,7 @@ public class TransactionServices {
 
 
     public Transaction apiSave(Transaction transaction){
-        int acknowledge=jdbcTemplate.update("insert into transaction_table() values(?,?,?,?,?,?)",
+        int acknowledge=jdbcTemplate.update("insert into transaction() values(?,?,?,?,?,?)",
                 new Object[]{
                         transaction.getTransactionId(),
                         transaction.getTransactionDate(),
@@ -28,7 +28,7 @@ public class TransactionServices {
     }
 
     public List<Transaction> apiFindBySender(String sender){
-        List<Transaction> myCards= (List<Transaction>) jdbcTemplate.query("select * from transaction_table where transaction_by=?",
+        List<Transaction> myCards= (List<Transaction>) jdbcTemplate.query("select * from transaction where transaction_by=?",
                 new Object[]{sender},
                 new BeanPropertyRowMapper<>(Transaction.class));
         return myCards;
@@ -36,14 +36,14 @@ public class TransactionServices {
 
 
     public List<Transaction> apiFindByReceiver(String receiver){
-        List<Transaction> myCards= (List<Transaction>) jdbcTemplate.query("select * from transaction_table where transaction_to=?",
+        List<Transaction> myCards= (List<Transaction>) jdbcTemplate.query("select * from transaction where transaction_to=?",
                 new Object[]{receiver},
                 new BeanPropertyRowMapper<>(Transaction.class));
         return myCards;
     }
 
     public List<Transaction> apiFindByAmount(Integer amount){
-        List<Transaction> myCards= (List<Transaction>) jdbcTemplate.query("select * from transaction_table where transaction_amount=?",
+        List<Transaction> myCards= (List<Transaction>) jdbcTemplate.query("select * from transaction where transaction_amount=?",
                 new Object[]{amount},
                 new BeanPropertyRowMapper<>(Transaction.class));
         return myCards;
